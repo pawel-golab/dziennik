e73ee9877898dd13db33f186aca8b1b459799ac9
Paweł <pawelgolab2001@gmail.com>
2021-08-21 22:55:54 +0200

    calendar beta 1.1.0

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <link rel="stylesheet" href="bootstrap-5/css/bootstrap.min.css"> <!--bootstrap css-->
+    <link rel="stylesheet" href="bootstrap-5/css/bootstrap.min.css"> <!-- bootstrap css -->
     <script src="https://kit.fontawesome.com/d5272c5ae6.js" crossorigin="anonymous"></script> <!--fontawesome-->
     <link rel="stylesheet" href="style/main.css">
     <script src="./js-scripts/jQuery.js"></script>
@@ -24,18 +24,17 @@
 <?php require './includes/header.html'; ?>
 <div id="calendar"></div>
 <script>
-    <?php require './js-scripts/calendar.js'; ?>
 
     let date = new Date;
     let m   = date.getMonth();      //month
     let y   = date.getFullYear();   //year
 
     //testowanie:
+    
     generateCalendar(
         y, m+1, '#calendar',
         ['','span','<div>'],                 //tags
-        ['main-2','day-number','day-tasks','accent'],    //classes
-        [0,0,0,'filter: brightness(1.2)'], //styles
+        ['main-2','day-number','day-tasks','accent', 'error'],    //classes
+        [0,0,0,'filter: brightness(1.2);','font-style: italic'], //styles
     );
-    //test
 </script>
